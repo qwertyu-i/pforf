eb9879a6859473bcb4202d8e37e255cf614466e1
qwertyu <luke.z24680@gmail.com>
2026-01-09 22:35:23 -0500

    finished writing a few primitives and tests for them

---
+++ test.py
@@ -0,0 +1,8 @@
+from pforf import Pforf
+# for now, just set code as string
+# all of these should return 4
+p = Pforf()
+p.run("2 2 +")
+p.run("-2 ABS 2 +")
+p.run("2 ?DUP 0 ?DUP + +")
+p.run("1 1+ 1+ 1+")
