0d23b4b681bc318054fc5a5752b8ea292a11bfbb
qwertyu <luke.z24680@gmail.com>
2026-01-11 17:54:15 -0500

    added do loops fully with I

--- pforf.py
+++ pforf.py
@@ -11,6 +11,8 @@ class Pforf:
         self.ip = 0
         # the tokens made from code
         self.tokens = []
+        # Added: stack to handle nested loops (index, limit, start_ip)
+        self.loop_stack = []
 
         # stores the primitive words (functions)
         # primitives implemented are based on the following list:
@@ -52,13 +54,15 @@ class Pforf:
             "?DUP": lambda: self.stack[-1] != 0 and self.stack.append(self.stack[-1]),
             "ABS": lambda: self.stack.append(abs(self.stack.pop())),
             "AND": lambda: self.stack.append(1 if self.stack.pop() == self.stack.pop() else 0),
-            "IF": lambda: self.ifWord(),
+            "IF": self.ifWord,
             # THEN is handled by IF
-            "THEN": lambda: self.thenWord(),
-            "ELSE": lambda: None,
-            "DO": lambda: self.doWord(),
-            "LOOP": lambda: None
+            "THEN": self.thenWord,
+            "ELSE": lambda: None, # terminator
+            "DO": self.doWord,
+            "LOOP": self.loopWord,
+            "I": self.iWord
         }
+
     def skip_until(self, targets):
         # increase when we find another if since we don't want to end early
         depth = 0
@@ -66,25 +70,21 @@ class Pforf:
             self.ip += 1
             token = self.tokens[self.ip]
 
-            if token == "IF":
+            # Track depth for nesting (IFs and DOs)
+            if token in ["IF", "DO"]:
                 depth += 1
             elif token == "ELSE":
                 if depth == 0 and "ELSE" in targets:
                     return
                 depth -= 1
-            elif token == "THEN" and depth == 0 and "THEN" in targets:
-                return
+            elif token == "THEN":
+                if depth == 0 and "THEN" in targets:
+                    return
+            elif token == "LOOP":
+                depth -= 1
 
         raise SyntaxError(f"Missing terminator {targets}")
 
-    def searchNext(self, word):
-        searchArea = self.tokens[self.ip:]
-        index = 0
-        for token in searchArea:
-            if token == word:
-                return index
-            index += 1
-        
     def ifWord(self):
         if self.stack.pop() == 0:
             self.skip_until(["THEN", "ELSE"])
@@ -94,31 +94,36 @@ class Pforf:
 
     def doWord(self):
         index = self.stack.pop()
-        ceil = self.stack.pop()
-        start = self.ip + 1
-        end = self.searchNext("LOOP") + start - 1
-        loopTokens = self.tokens[start:end]
-        loopIp = 0
-        # turn this loop into an eval function
-        while index < ceil:
-            while loopIp < len(loopTokens):
-                token = loopTokens[loopIp]
-                if token in self.dictionary:
-                    self.dictionary[token]()
-                else:
-                    try:
-                        # type check later, adds unrecognised tokens as floats
-                        self.stack.append(float(token))
-                    except ValueError:
-                        raise SyntaxError(f"Unknown word: {token}")
-                loopIp += 1
-            loopIp = 0
-            index += 1
-        self.ip = end
+        limit = self.stack.pop()
+        # Save the loop state on a stack for nesting
+        self.loop_stack.append({
+            "index": index,
+            "limit": limit,
+            "start": self.ip + 1
+        })
+
+    def iWord(self):
+        if not self.loop_stack:
+            raise SyntaxError("I used outside of a loop")
+        self.stack.append(self.loop_stack[-1]["index"])
+
+    def loopWord(self):
+        if not self.loop_stack:
+            raise SyntaxError("LOOP without DO")
+        
+        loop = self.loop_stack[-1]
+        loop["index"] += 1
+        
+        if loop["index"] < loop["limit"]:
+            # -1 because run method will add 1 immediately
+            self.ip = loop["start"] - 1
+        else:
+            self.loop_stack.pop()
 
     def run(self, code):
         self.ip = 0
         self.stack = []
+        self.loop_stack = []
         # code is just whitespace separated so we can use split() to read it
         self.tokens = code.split()
         while self.ip < len(self.tokens):
@@ -133,15 +138,5 @@ class Pforf:
                     raise SyntaxError(f"Unknown word: {token}")
             self.ip += 1
 
-        # old loop
-        # for token in tokens:
-        #     if token in self.dictionary:
-        #         self.dictionary[token]()
-        #     else:
-        #         try:
-        #             # type check later, adds unrecognised tokens as floats
-        #             self.stack.append(float(token))
-        #         except ValueError:
-        #             print(f"Unknown word: {token}")
         # just print the stack at the end for now
         print(self.stack)
