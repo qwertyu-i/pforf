8c4ec9c5b757ebc360832e37157d35f7579bc9f5
qwertyu <luke.z24680@gmail.com>
2026-01-11 21:34:38 -0500

    got everything finished and started creating a beer on the wall test

--- pforf.py
+++ pforf.py
@@ -1,7 +1,8 @@
 # Interpreter for a forth-like language (not following any specific standards)
 #                                        ^--(this is why it's forf not forth)
 
-#TODO: evaluation function, coldef
+# for getting files
+import sys
 
 class Pforf:
     def __init__(self):
@@ -49,37 +50,56 @@ class Pforf:
             # also no words for controlling memory will be added
             "+": lambda: self.stack.append(self.stack.pop(-2) + self.stack.pop(-1)),
             "-": lambda: self.stack.append(self.stack.pop(-2) - self.stack.pop(-1)),
+            "SWAP": self.swapWord,
             # duplicates head if non-zero
             # also a cool trick to append conditionally i got from ai
             "?DUP": lambda: self.stack[-1] != 0 and self.stack.append(self.stack[-1]),
+            "DUP": lambda: self.stack.append(self.stack[-1]),
             "ABS": lambda: self.stack.append(abs(self.stack.pop())),
             "AND": lambda: self.stack.append(1 if self.stack.pop() == self.stack.pop() else 0),
+            "OR": lambda: self.stack.append(1 if self.stack.pop() == True or self.stack.pop() == True else 0),
             "IF": self.ifWord,
             # THEN is handled by IF
             "THEN": self.thenWord,
-            "ELSE": lambda: None, # terminator
+            "ELSE": lambda: None,
             "DO": self.doWord,
             "LOOP": self.loopWord,
-            "I": self.iWord
+            "I": self.iWord,
+            ":": self.colWord,
+            ";": lambda: None,
+            ".\"": self.quoteWord,
+            "\"": None,
+            ".": lambda: print(self.stack.pop(), end = ""),
+            "CR": lambda: print(),
+            "SPACE": lambda: print(" ", end = "")
         }
 
+        # definitions will be "WORD: thread"
+        # this takes priority over primitives
+        self.wordDictionary = {}
+
+
+    def swapWord(self):
+        a = self.stack.pop()
+        b = self.stack.pop()
+        self.stack.append(a)
+        self.stack.append(b)
+
     def skip_until(self, targets):
-        # increase when we find another if since we don't want to end early
         depth = 0
         while self.ip < len(self.tokens) - 1:
             self.ip += 1
             token = self.tokens[self.ip]
 
-            # Track depth for nesting (IFs and DOs)
+            # 1. Check if we found our target at the CURRENT nesting level
+            if depth == 0 and token in targets:
+                return
+
+            # 2. Track nesting depth
             if token in ["IF", "DO"]:
                 depth += 1
-            elif token == "ELSE":
-                if depth == 0 and "ELSE" in targets:
-                    return
-                depth -= 1
             elif token == "THEN":
-                if depth == 0 and "THEN" in targets:
-                    return
+                depth -= 1
             elif token == "LOOP":
                 depth -= 1
 
@@ -87,7 +107,7 @@ class Pforf:
 
     def ifWord(self):
         if self.stack.pop() == 0:
-            self.skip_until(["THEN", "ELSE"])
+            self.skip_until(["THEN"])
 
     def thenWord(self):
         self.skip_until(["ELSE"])
@@ -120,6 +140,74 @@ class Pforf:
         else:
             self.loop_stack.pop()
 
+    def colWord(self):
+        self.ip += 1
+        if self.ip >= len(self.tokens):
+            raise SyntaxError("Expected word name after ':'")
+        
+        word_name = self.tokens[self.ip]
+        definition = []
+        
+        self.ip += 1
+        while self.ip < len(self.tokens):
+            token = self.tokens[self.ip]
+            if token == ";":
+                self.wordDictionary[word_name] = definition
+                return
+            definition.append(token)
+            self.ip += 1
+            
+        raise SyntaxError("Missing ';' in colon definition")
+
+    def execute_tokens(self, tokens_to_run):
+        old_tokens = self.tokens
+        old_ip = self.ip
+        
+        # treat this as a new "file" while keeping data stack
+        self.tokens = tokens_to_run
+        self.ip = 0
+        
+        while self.ip < len(self.tokens):
+            token = self.tokens[self.ip]
+            self.evaluate_token(token)
+            self.ip += 1
+            
+        # Restore state
+        self.tokens = old_tokens
+        self.ip = old_ip
+
+    def evaluate_token(self, token):
+        if token in self.wordDictionary:
+            # keeps going until primitives reached
+            self.execute_tokens(self.wordDictionary[token])
+        elif token in self.dictionary:
+            self.dictionary[token]()
+        else:
+            try:
+                if "." in token:
+                    self.stack.append(float(token))
+                else:
+                    self.stack.append(int(token))
+            except ValueError:
+                raise SyntaxError(f"Unknown word: {token}")
+
+    def quoteWord(self):
+        self.ip += 1
+        start_ip = self.ip
+        
+        # go through until finding the ending word, because
+        # forth likes to have the first quote as a word by itself
+        # then the ending quote as part of the last word??
+        # so we have to check the end of each token
+        while self.ip < len(self.tokens):
+            if self.tokens[self.ip].endswith('"'):
+                full_string = " ".join(self.tokens[start_ip : self.ip + 1])
+                self.stack.append(full_string.rstrip('"'))
+                return
+            self.ip += 1
+            
+        raise SyntaxError("Missing closing quote for string")
+
     def run(self, code):
         self.ip = 0
         self.stack = []
@@ -127,16 +215,21 @@ class Pforf:
         # code is just whitespace separated so we can use split() to read it
         self.tokens = code.split()
         while self.ip < len(self.tokens):
-            token = self.tokens[self.ip]
-            if token in self.dictionary:
-                self.dictionary[token]()
-            else:
-                try:
-                    # type check later, adds unrecognised tokens as floats
-                    self.stack.append(float(token))
-                except ValueError:
-                    raise SyntaxError(f"Unknown word: {token}")
+            self.evaluate_token(self.tokens[self.ip])
             self.ip += 1
+        print()
 
-        # just print the stack at the end for now
-        print(self.stack)
+p = Pforf()
+if __name__ == "__main__":
+    interpreter = Pforf()
+
+    if len(sys.argv) > 1:
+        filepath = sys.argv[1]
+        try:
+            with open(filepath, 'r') as file:
+                code = file.read()
+                interpreter.run(code)
+        except FileNotFoundError:
+            print(f"Error: The file '{filepath}' was not found.")
+    else:
+        print("Usage: python pforf.py <filename.pf>")
